c7c16e1da917dbb11aff395af790406836e40881
MaximKovalski <nobesko@gmail.com>
2023-05-31 19:38:20 +0300

    practice

---
+++ javascript.js
@@ -0,0 +1,80 @@
+// exercise 1 
+// console.log('exercise 1')
+// let bob = 'bob'
+// sayHello('bob')
+// function sayHello(name){
+//     console.log(`the name is ${name}`)
+// }
+// exercise 2
+// console.log('exercise 2')
+// function sayHelloToBob(name){
+// let name = 'bob'
+// }
+// console.log(`hello ${name}`)
+
+// exercise 3 CallBack Function 
+// console.log('exercise 3')
+// function greet(name){
+//     console.log(`hello ${name}`)
+// }
+// function useGreet(name,callback){
+//     callback(name)
+// }
+// useGreet('bob',greet)
+
+
+// function add(number1,number2){
+//     console.log((number1)+(number2))
+// }
+// function subtract(number1,number2){
+//     console.log((number1)-(number2))
+// }
+// function multiply(number1,number2){
+//     console.log((number1) * (number2))
+// }
+// function divide(number1,number2){
+// console.log(number1 / number2)
+// }
+// function numbers(number1,number2,mathFun){
+//     mathFun(number1,number2)
+// }
+// numbers(2,5,subtract)
+
+// const calc = (operator,number1,number2)=>{
+//     switch(operator){
+//         case "+":
+//             return number1+number2;
+//             case "-":
+//                 return number1-number2;
+//                 case "*":
+//                     return number1*number2;
+//                     case "/":
+//                         return number1 / number2;
+//                         default:
+//                             return 'invalid operator'
+//     }
+// }
+// console.log(calc('+',5,7))
+
+
+// let a =  [1,3,7,4,2,9];
+// let sum = 0
+// for (let i = 0; i < a.length; i++) {
+//  const element = a[i] + 1
+//  sum += element
+// }
+// console.log(sum)
+
+// let a =  [1,3,7,4,2,9];
+// let sum = 0
+// for(let number in a){
+// sum += a[number]
+// }
+// console.log(sum)
+
+// let b =['reet','teewt','tersdf']
+// for(let s in b ){
+//     console.log(b[s])
+// }
+
+
